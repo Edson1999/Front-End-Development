20c00a0d74dfd52ea86d3471364107bb3cddf566
Edson Ibarra <EdsonP4100@gmail.com>
2023-09-19 14:22:00 -0700

    Update new form validation

--- UpTask_MERN/frontend/src/pages/Register.jsx
+++ UpTask_MERN/frontend/src/pages/Register.jsx
@@ -1,13 +1,61 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import Alerta from '../components/Alerta';
 
 export const Register = () => {
+  const [nombre, setNombre] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [repetirPassword, setRepetirPassword] = useState('');
+
+  const [alerta, setAlerta] = useState({});
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if ([nombre, email, password, repetirPassword].includes('')) {
+      setAlerta({
+        msg: 'Todos los campos son obligatorios',
+        error: true,
+      });
+      return;
+    }
+
+    if (password !== repetirPassword) {
+      setAlerta({
+        msg: 'Las contraseñas no coinciden',
+        error: true,
+      });
+      return;
+    }
+
+    if (password.length < 6) {
+      setAlerta({
+        msg: 'El password debe tener más de 6 caracteres',
+        error: true,
+      });
+      return;
+    }
+
+    setAlerta({});
+
+    // Create user in API
+    // Send api request to create users
+  };
+
+  const { msg } = alerta;
+
   return (
     <>
       <h1 className="text-blue-600 font-black text-5xl capitalize mt-8">
         Crea tú cuenta y administra tus{' '}
         <span className="text-slate-600">proyectos</span>
       </h1>
-      <form className="my-8 bg-white shadow rounded-3xl py-2 px-4">
+      {msg && <Alerta alerta={alerta} />}
+      <form
+        onSubmit={handleSubmit}
+        className="my-8 bg-white shadow rounded-3xl py-2 px-4"
+      >
         <div className="my-5">
           <label htmlFor="nombre" className="text-gray-600 block font-bold">
             Nombre
@@ -17,6 +65,8 @@ export const Register = () => {
             type="text"
             placeholder="Ingresa tú nombre"
             className="w-full mt-2 py-2 px-4 rounded-3xl border bg-gray-100"
+            value={nombre}
+            onChange={(e) => setNombre(e.target.value)}
           />
         </div>
         <div className="my-5">
@@ -28,6 +78,8 @@ export const Register = () => {
             type="email"
             placeholder="Email de registro"
             className="w-full mt-2 py-2 px-4 rounded-3xl border bg-gray-100"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
         <div className="my-5">
@@ -39,6 +91,8 @@ export const Register = () => {
             type="password"
             placeholder="Password de registro"
             className="w-full mt-2 py-2 px-4 rounded-3xl border bg-gray-100"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
         <div className="my-5">
@@ -53,6 +107,8 @@ export const Register = () => {
             type="password"
             placeholder="Repetir tú Password"
             className="w-full mt-2 py-2 px-4 rounded-3xl border bg-gray-100"
+            value={repetirPassword}
+            onChange={(e) => setRepetirPassword(e.target.value)}
           />
         </div>
         <input
